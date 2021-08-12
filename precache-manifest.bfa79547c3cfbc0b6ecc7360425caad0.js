5adbeaa112109647ba0d8fcb235af3c7aca77fe0
Mariusz Rakus <mariusz@raqz.pl>
2021-08-12 18:08:21 +0200

    Updates

---
+++ precache-manifest.bfa79547c3cfbc0b6ecc7360425caad0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56642dbed21c7f5104575a72674c4461",
+    "url": "./index.html"
+  },
+  {
+    "revision": "edb12eeb0d3c945380b0",
+    "url": "./static/css/2.f10b9517.chunk.css"
+  },
+  {
+    "revision": "eeb5aef3183be5c5b5ba",
+    "url": "./static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "edb12eeb0d3c945380b0",
+    "url": "./static/js/2.d62d1511.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d62d1511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb5aef3183be5c5b5ba",
+    "url": "./static/js/main.090623d7.chunk.js"
+  },
+  {
+    "revision": "7b4b9dccd1d61d7bb3a9",
+    "url": "./static/js/runtime-main.2574dcc2.js"
+  }
+]);
